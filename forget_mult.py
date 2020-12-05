cb8f79098de5077540427cbb03be84be3593d293
liangDYL <41284007+liangDYL@users.noreply.github.com>
2020-12-05 14:44:56 +0800

    I have updated the project to pytorch 1.7

--- forget_mult.py
+++ forget_mult.py
@@ -170,70 +170,6 @@ class GPUForgetMult(torch.autograd.Function):
             return grad_f, grad_x, grad_h_init
         return grad_f, grad_x
 
-
-class GPUForgetMult_copy(torch.autograd.Function):
-    configured_gpus = {}
-    ptx = None
-    def __init__(self):
-        super(GPUForgetMult, self).__init__()
-
-    def compile(self):
-        if self.ptx is None:
-            program = Program(kernel.encode(), 'recurrent_forget_mult.cu'.encode())
-            GPUForgetMult.ptx = program.compile()
-
-        if torch.cuda.current_device() not in GPUForgetMult.configured_gpus:
-            m = function.Module()
-            m.load(bytes(self.ptx.encode()))
-
-            self.forget_mult = m.get_function('recurrent_forget_mult')
-            self.bwd_forget_mult = m.get_function('bwd_recurrent_forget_mult')
-
-            Stream = namedtuple('Stream', ['ptr'])
-            self.stream = Stream(ptr=torch.cuda.current_stream().cuda_stream)
-
-            GPUForgetMult.configured_gpus[torch.cuda.current_device()] = (self.forget_mult, self.bwd_forget_mult, self.stream)
-
-        self.forget_mult, self.bwd_forget_mult, self.stream = GPUForgetMult.configured_gpus[torch.cuda.current_device()]
-
-    def forward(self, f, x, hidden_init=None):
-        self.compile()
-        seq_size, batch_size, hidden_size = f.size()
-        result = f.new(seq_size + 1, batch_size, hidden_size)
-        # We only zero the result array (result[0]) if we don't set a hidden initial state
-        # All other values (result[1:]) are overwritten by default
-        if hidden_init is not None: result[0, :, :] = hidden_init
-        else: result = result.zero_()
-        ###
-        grid_hidden_size = min(hidden_size, 512)
-        grid = (math.ceil(hidden_size / grid_hidden_size), batch_size)
-        self.forget_mult(grid=grid, block=(grid_hidden_size, 1), args=[result.data_ptr(), f.data_ptr(), x.data_ptr(), seq_size, batch_size, hidden_size], stream=self.stream)
-        self.save_for_backward(f, x, hidden_init)
-        self.result = result
-        return result[1:, :, :]
-
-    def backward(self, grad_h):
-        self.compile()
-        f, x, hidden_init = self.saved_tensors
-        h = self.result
-        ###
-        seq_size, batch_size, hidden_size = f.size()
-        # Zeroing is not necessary as these will be overwritten
-        grad_f = f.new(*f.size())
-        grad_x = f.new(*f.size())
-        grad_h_init = f.new(batch_size, hidden_size)
-        ###
-        grid_hidden_size = min(hidden_size, 512)
-        grid = (math.ceil(hidden_size / grid_hidden_size), batch_size)
-        self.bwd_forget_mult(grid=grid, block=(grid_hidden_size, 1), args=[h.data_ptr(), f.data_ptr(), x.data_ptr(), grad_h.data_ptr(), grad_f.data_ptr(), grad_x.data_ptr(), grad_h_init.data_ptr(), seq_size, batch_size, hidden_size], stream=self.stream)
-        ###
-        if hidden_init is not None:
-            return grad_f, grad_x, grad_h_init
-        return grad_f, grad_x
-
-
-
-
 class ForgetMult(torch.nn.Module):
     r"""ForgetMult computes a simple recurrent equation:
     h_t = f_t * x_t + (1 - f_t) * h_{t-1}
@@ -250,29 +186,14 @@ class ForgetMult(torch.nn.Module):
     def __init__(self):
         super(ForgetMult, self).__init__()
 
-    def forward_copy(self, f, x, hidden_init=None, use_cuda=True):
-        # Use CUDA by default unless it's available
-        use_cuda = use_cuda and torch.cuda.is_available()
-        print(use_cuda)
-        # Ensure the user is aware when ForgetMult is not GPU version as it's far faster
-        if use_cuda: assert f.is_cuda and x.is_cuda, 'GPU ForgetMult with fast element-wise CUDA kernel requested but tensors not on GPU'
-        ###
-        # Avoiding 'RuntimeError: expected a Variable argument, but got NoneType' when hidden_init is None
-        if hidden_init is None: return GPUForgetMult()(f, x) if use_cuda else CPUForgetMult()(f, x)
-        return GPUForgetMult()(f, x, hidden_init) if use_cuda else CPUForgetMult()(f, x, hidden_init)
-
     def forward(self, f, x, hidden_init=None, use_cuda=True):
         # Use CUDA by default unless it's available
         use_cuda = use_cuda and torch.cuda.is_available()
-        # print(use_cuda)
         # Ensure the user is aware when ForgetMult is not GPU version as it's far faster
         if use_cuda: assert f.is_cuda and x.is_cuda, 'GPU ForgetMult with fast element-wise CUDA kernel requested but tensors not on GPU'
         ###
         # Avoiding 'RuntimeError: expected a Variable argument, but got NoneType' when hidden_init is None
-        # ptx=None
-        # configured_gpus={}
-        if hidden_init is None: return GPUForgetMult.apply(f, x,ptx,configured_gpus) if use_cuda else CPUForgetMult.apply(f, x)
-        # return GPUForgetMult.apply(f, x,ptx,configured_gpus, hidden_init) if use_cuda else CPUForgetMult()(f, x, hidden_init)
+        if hidden_init is None: return GPUForgetMult.apply(f, x) if use_cuda else CPUForgetMult()(f, x)
         return GPUForgetMult.apply(f, x, hidden_init) if use_cuda else CPUForgetMult()(f, x, hidden_init)
 ###
 
